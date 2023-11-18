44df617e985c2d2d4137d1a73605b355e25f0847
dezo103 <dezo103@gmail.com>
2023-11-18 11:31:15 +0300

    feat: add card component

---
+++ src/components/ui/card/card.stories.tsx
@@ -0,0 +1,47 @@
+import type { Meta, StoryObj } from '@storybook/react'
+
+import { Button } from '@/components/ui/button'
+import { Typography } from '@/components/ui/typography'
+
+// eslint-disable-next-line import/extensions
+import { Card } from './index.ts'
+
+const meta = {
+  argTypes: {},
+  component: Card,
+  tags: ['autodocs'],
+  title: 'Components/Card',
+} satisfies Meta<typeof Card>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const CardWithButtons: Story = {
+  args: {
+    children: [
+      <Button variant={'tertiary'}>Button in the card</Button>,
+      <Button variant={'primary'}>Button in the card</Button>,
+      <Button variant={'secondary'}>Button in the card</Button>,
+      <Button variant={'link'}>Button in the card</Button>,
+    ],
+  },
+}
+export const CardWithTypography: Story = {
+  args: {
+    children: [
+      <Typography variant={'body1'}>Some text</Typography>,
+      <Typography variant={'body2'}>Some text</Typography>,
+      <Typography variant={'caption'}>Some text</Typography>,
+      <Typography variant={'large'}>Some text</Typography>,
+      <Typography as={'a'} variant={'link1'}>
+        Some text
+      </Typography>,
+      <Typography as={'a'} variant={'link2'}>
+        Some text
+      </Typography>,
+      <Typography variant={'overline'}>Some text</Typography>,
+      <Typography variant={'subtitle1'}>Some text</Typography>,
+      <Typography variant={'subtitle2'}>Some text</Typography>,
+    ],
+  },
+}
---
+++ src/components/ui/card/card.tsx
@@ -0,0 +1,13 @@
+import { ReactNode } from 'react'
+
+import s from './card.module.scss'
+
+export type CardProps = {
+  children?: ReactNode[]
+  className?: string
+}
+export const Card = (props: CardProps) => {
+  const { className, ...rest } = props
+
+  return <div className={`${s.card} ${className}`} {...rest}></div>
+}
