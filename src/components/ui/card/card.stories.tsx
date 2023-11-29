aedf8a31a90650a37efb9028aa471ac4cde0c4b2
dezo103 <dezo103@gmail.com>
2023-11-29 21:50:14 +0300

    ref: refactor Card component

--- src/components/ui/card/card.stories.tsx
+++ src/components/ui/card/card.stories.tsx
@@ -18,51 +18,55 @@ type Story = StoryObj<typeof meta>
 
 export const CardWithButtons: Story = {
   args: {
-    children: [
-      <Button variant={'tertiary'}>Button in the card</Button>,
-      <Button variant={'primary'}>Button in the card</Button>,
-      <Button variant={'secondary'}>Button in the card</Button>,
-      <Button variant={'link'}>Button in the card</Button>,
-    ],
+    children: (
+      <>
+        <Button variant={'tertiary'}>Button in the card</Button>
+        <Button variant={'primary'}>Button in the card</Button>
+        <Button variant={'secondary'}>Button in the card</Button>
+        <Button variant={'link'}>Button in the card</Button>
+      </>
+    ),
   },
 }
 export const CardWithTypography: Story = {
   args: {
-    children: [
-      <Typography variant={'body1'}>Some text</Typography>,
-      <Typography variant={'body2'}>Some text</Typography>,
-      <Typography variant={'caption'}>Some text</Typography>,
-      <Typography variant={'large'}>Some text</Typography>,
-      <Typography as={'a'} variant={'link1'}>
-        Some text
-      </Typography>,
-      <Typography as={'a'} variant={'link2'}>
-        Some text
-      </Typography>,
-      <Typography variant={'overline'}>Some text</Typography>,
-      <Typography variant={'subtitle1'}>Some text</Typography>,
-      <Typography variant={'subtitle2'}>Some text</Typography>,
-    ],
+    children: (
+      <>
+        <Typography variant={'body1'}>Some text</Typography>
+        <Typography variant={'body2'}>Some text</Typography>
+        <Typography variant={'caption'}>Some text</Typography>
+        <Typography variant={'large'}>Some text</Typography>
+        <Typography as={'a'} variant={'link1'}>
+          Some text
+        </Typography>
+        <Typography as={'a'} variant={'link2'}>
+          Some text
+        </Typography>
+        <Typography variant={'overline'}>Some text</Typography>
+        <Typography variant={'subtitle1'}>Some text</Typography>
+        <Typography variant={'subtitle2'}>Some text</Typography>
+      </>
+    ),
   },
 }
 
 export const ArticleCard: Story = {
   args: {
     as: 'article',
-    children: ['ArticleCard'],
+    children: 'ArticleCard',
   },
 }
 
 export const SectionCard: Story = {
   args: {
     as: 'section',
-    children: ['SectionCard'],
+    children: 'SectionCard',
   },
 }
 
 export const StyledCard: Story = {
   args: {
-    children: ['StyledCard'],
+    children: 'StyledCard',
     style: {
       backgroundColor: 'lightgray',
       height: '480px',
