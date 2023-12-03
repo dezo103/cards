b407b99bca8a42d3d228b0099016c69e01998d52
dezo103 <dezo103@gmail.com>
2023-12-03 23:01:09 +0300

    feat: add userDropdown styles

--- src/components/layout/header/user-dropdown.tsx
+++ src/components/layout/header/user-dropdown.tsx
@@ -3,6 +3,7 @@ import { PersonOutlined } from '@/assets/images/personOutlined'
 import { DropdownCustomMenu } from '@/components/ui/dropdownMenu'
 import { DropdownItem } from '@/components/ui/dropdownMenu/dropdownItem'
 import { DropdownSeparator } from '@/components/ui/dropdownMenu/dropdownSeparator'
+import { DropdownUserLabel } from '@/components/ui/dropdownMenu/dropdownUserLabel'
 import { Typography } from '@/components/ui/typography'
 
 import userLogo from '../../../assets/images/userLogo.png'
@@ -10,6 +11,8 @@ import userLogo from '../../../assets/images/userLogo.png'
 export const UserDropdown = () => {
   return (
     <DropdownCustomMenu trigger={<img alt={'user'} src={userLogo} />}>
+      <DropdownUserLabel />
+      <DropdownSeparator />
       <DropdownItem>
         <PersonOutlined />
         <Typography style={{ color: '#fff' }} variant={'caption'}>
