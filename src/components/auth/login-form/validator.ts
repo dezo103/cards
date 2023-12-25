3a6341a0987d625dfa72aa4f73742257388cf695
dezo103 <dezo103@gmail.com>
2023-12-25 12:29:29 +0300

    ref: refactor validation

--- src/components/auth/login-form/validator.ts
+++ src/components/auth/login-form/validator.ts
@@ -1,7 +1,7 @@
 import { z } from 'zod'
 
 export const loginSchema = z.object({
-  email: z.string().email(),
+  email: z.string().email('custom email error message'), // if we need to show custom error message
   password: z.string().min(3),
-  rememberMe: z.boolean().default(false),
+  rememberMe: z.boolean().default(false).optional(),
 })
